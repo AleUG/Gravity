f9b55946d96e89e969e4750796dc128e49ad98e0
AleUG <alejandro.urrutiag@estudiantes.arcos.cl>
2024-04-03 21:39:22 -0300

    Proyecto_0.0.1

--- Assets/Scripts/ChangeGravity.cs
+++ Assets/Scripts/ChangeGravity.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine.Rendering;
+using Unity.VisualScripting;
 
 public class ChangeGravity : MonoBehaviour
 {
     private Rigidbody2D rb;
+
+    public Animator animator;
+
     public bool lowGravity;
     public bool highGravity;
     public bool neutralGravity;
@@ -16,17 +23,32 @@ public class ChangeGravity : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player") && lowGravity)
+        if (collision.CompareTag("Player") && lowGravity)
         {
-            rb.gravityScale = 1.0f;
+            rb.gravityScale = 0.75f;
+            animator.SetBool("low", true);
+
+
         }
         else if (collision.CompareTag("Player") && highGravity)
         {
             rb.gravityScale = 4.0f;
+            animator.SetBool("high", true);
         }
-        else if(collision.CompareTag("Player") && neutralGravity)
+        else if (collision.CompareTag("Player") && neutralGravity)
         {
             rb.gravityScale = 2.0f;
+            animator.SetBool("none", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            animator.SetBool("low", false);
+            animator.SetBool("high", false);
+            animator.SetBool("none", false);
         }
     }
 }
