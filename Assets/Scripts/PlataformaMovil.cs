480d0ee188efec81df53facaab4e336b45c760ad
AleUG <alejandro.urrutiag@estudiantes.arcos.cl>
2024-05-02 18:14:23 -0400

    Proyecto_0.0.10

---
+++ Assets/Scripts/PlataformaMovil.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+public class PlataformaMovil : MonoBehaviour
+{
+    public Transform splineAnimateObject;
+    public Rigidbody2D rb2D;
+
+    Vector3 velocidad;
+    Vector3 prevPos;
+
+    private void Awake()
+    {
+
+        rb2D = GetComponentInChildren<Rigidbody2D>();
+        prevPos = splineAnimateObject.position;
+    }
+
+    private void FixedUpdate()
+    {
+        velocidad = (splineAnimateObject.position - prevPos) / Time.deltaTime;
+        prevPos = splineAnimateObject.position;
+
+        rb2D.velocity = velocidad;
+
+    }
+}
